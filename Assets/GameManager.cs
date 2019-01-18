76fdf01c0948520d06291bc1e2972a65d0638808
Diego Blank <diegobrunoblank@gmail.com>
2019-01-18 12:19:02 +0100

    Added Challenge System and Radio

---
+++ Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public static int CurrentObjective = 1; //radio, level 1
+    public static bool isCompleted = false;
+
+    //level 1
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public static void CheckObjective() {
+
+        if (CurrentObjective == 1 && isCompleted == false) {
+
+            PlaySong.RadioSoundData.Play();
+            isCompleted = true;
+            CheckObjectiveCompletion();
+            
+
+        }
+
+        }
+
+    private static void CheckObjectiveCompletion() {
+        if (isCompleted == true) {
+            CurrentObjective += 1;
+            isCompleted = false;
+            Debug.Log(CurrentObjective);
+        }
+        
+    }
+
+
+
+    }
+
+
---
+++ Assets/PlaySong.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySong : MonoBehaviour {
+    public static AudioSource RadioSoundData;
+    public static bool isRadioPlaying = false;
+    private bool RadioActivatedOnce = false;
+
+    // Use this for initialization
+    void Start() {
+
+        RadioSoundData = GetComponent<AudioSource>();
+        
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RadioActivate() {
+
+        if (RadioActivatedOnce == false)
+        {
+            GameManager.CheckObjective();
+            RadioActivatedOnce = true;
+            Debug.Log(RadioActivatedOnce);
+        }
+
+        if (RadioActivatedOnce == true)
+            {
+                if (isRadioPlaying == false)
+                {
+                    RadioSoundData.Play();
+                    isRadioPlaying = true;
+
+                }
+                else if (isRadioPlaying == true)
+                {
+
+                    RadioSoundData.Stop();
+                    isRadioPlaying = false;
+                }
+
+
+            }
+
+    }
+}
