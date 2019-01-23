5267550ab0fcc611e65075b4918283de39b4bf13
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2019-01-23 15:27:03 +0100

    new characters

---
+++ Assets/fadeOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class fadeOnCollision : MonoBehaviour {
+
+	private SteamVR_Fade fadeScript;
+
+	private void Start() {
+		fadeScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<SteamVR_Fade>();
+	}
+
+	private void OnTriggerStay(Collider other) {
+		if (other.tag == "wall") {
+			fadeScript.OnStartFade(Color.green, 1f, true);
+		}
+	}
+}
