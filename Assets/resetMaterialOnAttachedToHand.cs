a928dcea96255f42e1db4c059486e46955dd5e93
Diego Blank <diegobrunoblank@gmail.com>
2019-02-14 23:33:44 +0100

    Highlight fix

---
+++ Assets/resetMaterialOnAttachedToHand.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class resetMaterialOnAttachedToHand : MonoBehaviour {
+
+    public GameObject handle;
+    private MeshRenderer rend;
+    private Material handleMat;
+
+    bool reset;
+
+	void Start () {
+        rend = handle.GetComponent<MeshRenderer>();
+        handleMat = rend.material;
+	}
+	
+    
+    public void resetMaterial()
+    {
+        reset = true;
+        
+    }
+
+    private void Update()
+    {
+        if (reset)
+        {
+            rend.material = handleMat;
+        }
+    }
+}
