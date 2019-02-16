51cd4a898083f2a3b44b9fdb926c5b022ba18c1d
Diego Blank <diegobrunoblank@gmail.com>
2019-02-17 00:19:12 +0100

    fixedMaterials

---
+++ Assets/randomBook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomBook : MonoBehaviour {
+
+    public Material[] mat;
+    private MeshRenderer meshRend;
+
+    void Start()
+    {
+        meshRend = transform.GetChild(0).GetComponent<MeshRenderer>();
+
+        Material applyMat = mat[Random.Range(0, mat.Length)];
+
+        meshRend.material = applyMat;
+    }
+}
